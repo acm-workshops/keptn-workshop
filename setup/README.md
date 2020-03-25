5ebae9bdfe718137ad52640679b39db7d68aa7ee
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-03-25 16:39:00 +0100

    adding ssl conf

---
+++ setup/README.md
@@ -0,0 +1,9 @@
+# 💻 ☁ Autonomous Cloud Workshop Setup of the environment
+
+> Let's **release better software faster** and put your IT on Autopilot with Keptn and Dynatrace. 
+> Let's build a Software delivery pipeline from scratch with quality gates and self-healing!
+ 
+ This folder contains the instructions to spin a linux machine and set everything automatic from scratch.
+
+The automatic setup of environments is done via this pyton application
+https://github.com/sergiohinojosa/Dynatrace-REST-Tenant-Automation/tree/app/keptn-6.1
---
+++ setup/expose-ssl-istio.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+printf "\nCreate Cluster-Issuer\n"
+kubectl apply -f clusterissuer.yaml
+
+
+printf "\nDeploy NGINX for the Certificate validation of the domain\n"
+printf "The Ingress is declared in one route of the ingress-ssl-istio\n"
+kubectl create deploy nginx --image=nginx
+kubectl expose deploy nginx --port=80 --type=NodePort
+
+printf "\nRouting Istio via Nginx Ingress, create secret and certificates for the valid endpoints\n"
+export PUBLIC_IP=$(curl -s ifconfig.me) 
+
+PUBLIC_IP_AS_DOM=$(echo $PUBLIC_IP | sed 's~\.~-~g')
+export DOMAIN="${PUBLIC_IP_AS_DOM}.nip.io"
+
+cat ingress.yaml | \
+  sed 's~domain.placeholder~'"$DOMAIN"'~' > ./gen/ingress-ssl-istio.yaml
+
+# Deploy ingress with rules to domains and ingress-gateway. Create secret and certificate
+kubectl apply -f gen/ingress-ssl-istio.yaml
+
+# LetsEncrypt Process
+printf " For observing the creation of the certificates: \n
+kubectl describe clusterissuers.cert-manager.io -A
+kubectl describe issuers.cert-manager.io -A
+kubectl describe certificates.cert-manager.io -A
+kubectl describe certificaterequests.cert-manager.io -A
+kubectl describe challenges.acme.cert-manager.io -A
+kubectl describe orders.acme.cert-manager.io -A
+kubectl get events
+"
