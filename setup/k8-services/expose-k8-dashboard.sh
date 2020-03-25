5c4a38f879184b9e125ddaa59ab98a5d5f6905a8
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-03-25 18:50:38 +0100

    adding k8 svcs

---
+++ setup/k8-services/expose-k8-dashboard.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+printf "\nExpose Dashboard\n"
+kubectl apply -f skip-login-in-k8-dashboard.yaml
+
+printf "\nExpose Kubernetes API and Dashboard\n"
+export PUBLIC_IP=$(curl -s ifconfig.me) 
+PUBLIC_IP_AS_DOM=$(echo $PUBLIC_IP | sed 's~\.~-~g')
+export DOMAIN="${PUBLIC_IP_AS_DOM}.nip.io"
+
+cat k8-svc-ingress.yaml | \
+  sed 's~domain.placeholder~'"$DOMAIN"'~' > ./gen/k8-svc-ingress.yaml
+
+# Deploy ingress with rules to domains and ingress-gateway. Create secret and certificate
+kubectl apply -f gen/k8-svc-ingress.yaml
