70cccfc1e108f4edddff2f747f88a7e62493e9d5
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-04-01 12:51:15 +0200

    renaming the connection to domain

--- setup/dynatrace/configure-k8.sh
+++ setup/dynatrace/configure-k8.sh
@@ -14,19 +14,27 @@ configureAccountAndGetCredentials(){
     wget https://www.dynatrace.com/support/help/codefiles/kubernetes/kubernetes-monitoring-service-account.yaml
     kubectl apply -f kubernetes-monitoring-service-account.yaml
     echo "\This is the SSL API URI of the K8s cluster exposed via nginx ingress:"
-    endpointUrl=$(kubectl get ing k8-api-ingress -o=jsonpath='{.spec.tls[0].hosts[0]}')
+    endpoint=$(kubectl get ing k8-api-ingress -o=jsonpath='{.spec.tls[0].hosts[0]}')
+    # Name the connection
+    title=$(echo $endpoint | sed 's~api\.kubernetes\.~~g' )
+    # Add protocol 
+    endpointUrl="https://${endpoint}"
     echo $endpointUrl
     echo "This is the Bearer Token for the K8s API:"
     authToken=$(kubectl get secret $(kubectl get sa dynatrace-monitoring -o jsonpath='{.secrets[0].name}' -n dynatrace) -o jsonpath='{.data.token}' -n dynatrace | base64 --decode)
     echo $authToken
 }
+
 replaceJson(){
     JSON=$(cat $FILE_IN)
     NJSON=$(echo $JSON | \
+    jq --arg title "$title" '.label = $title' | \
     jq --arg endpointUrl "$endpointUrl" '.endpointUrl = $endpointUrl' | \
     jq --arg authToken "$authToken" '.authToken = $authToken')
-    echo $NJSON > $FILE_OUT
+    echo $NJSON 
+    #echo $NJSON > $FILE_OUT
 }
+
 connectClusterWithDynatrace(){
     DESTINATION="https://$DT_TENANT/api/config/v1/kubernetes/credentials/?api-token=$DT_API_TOKEN"
     curl -v -X POST -H 'Content-Type: application/json' -H 'cache-control: no-cache' -d @$FILE_OUT $DESTINATION
