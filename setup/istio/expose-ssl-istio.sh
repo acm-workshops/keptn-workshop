42864584c5b763742390b3c1425dba718b1da0a4
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-04-01 10:35:21 +0200

    refactor and optimize

--- setup/istio/expose-ssl-istio.sh
+++ setup/istio/expose-ssl-istio.sh
@@ -1,4 +1,17 @@
 #!/bin/bash -x
+
+
+if [ $# -eq 1 ]; then
+    # Read JSON and set it in the CREDS variable 
+    DOMAIN=$1
+else
+    echo "No Domain has been passed, getting it from public ip"
+    export PUBLIC_IP=$(curl -s ifconfig.me) 
+    PUBLIC_IP_AS_DOM=$(echo $PUBLIC_IP | sed 's~\.~-~g')
+    export DOMAIN="${PUBLIC_IP_AS_DOM}.nip.io"
+    echo $DOMAIN
+fi
+
 printf "\nCreate Cluster-Issuer\n"
 kubectl apply -f clusterissuer.yaml
 
@@ -9,10 +22,6 @@ kubectl -n istio-system create deploy nginx --image=nginx
 kubectl -n istio-system expose deploy nginx --port=80 --type=NodePort
 
 printf "\nRouting Istio via Nginx Ingress, create secret and certificates for the valid endpoints\n"
-export PUBLIC_IP=$(curl -s ifconfig.me) 
-
-PUBLIC_IP_AS_DOM=$(echo $PUBLIC_IP | sed 's~\.~-~g')
-export DOMAIN="${PUBLIC_IP_AS_DOM}.nip.io"
 
 cat ingress-ssl-istio.yaml | \
   sed 's~domain.placeholder~'"$DOMAIN"'~' > ./gen/ingress-ssl-istio.yaml
