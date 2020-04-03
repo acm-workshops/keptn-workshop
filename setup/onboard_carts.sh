2a584cedd4dec7fc33062c808bf1f102916983e6
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-04-03 09:08:55 +0200

    onboarding QG

--- setup/onboard_carts.sh
+++ setup/onboard_carts.sh
@@ -4,13 +4,14 @@
 if [[ -d "carts" ]]; then
     # The context for this script needs to be in examples/onboarding-carts
     keptn create project sockshop --shipyard=./shipyard.yaml
+    # Onboarding - prepare  Keptn
     keptn onboard service carts --project=sockshop --chart=./carts
     keptn add-resource --project=sockshop --stage=dev --service=carts --resource=jmeter/basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
     keptn add-resource --project=sockshop --stage=staging --service=carts --resource=jmeter/load.jmx --resourceUri=jmeter/load.jmx
 
+    # Onboarding - prepare  Keptn
     keptn onboard service carts-db --project=sockshop --chart=./carts-db --deployment-strategy=direct
-    keptn send event new-artifact --project=sockshop --service=carts-db --image=docker.io/mongo --tag=4.2.2
-    keptn send event new-artifact --project=sockshop --service=carts --image=docker.io/keptnexamples/carts --tag=0.10.1
+
 else 
     echo "The helmcharts for carts are not present"
 fi 
